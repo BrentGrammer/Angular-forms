0c7e06976ddd9c700ebdf4ea36757b043efdf390
Brent Marquez <brentonmarquez@gmail.com>
2019-09-30 21:15:12 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  // get local ref for the form element:
+  @ViewChild("f") signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = "Superuser";
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log("Submitted", form);
+  // }
+  // alternative approach using @ViewChild to access form instead of passig it in
+  onSubmit() {
+    // access the form via the view child prop
+    console.log(this.signupForm);
+  }
+}
