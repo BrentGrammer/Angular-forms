8371b8346dd10708ce3b86ad6fa3d59761878bc6
Brent Marquez <brentonmarquez@gmail.com>
2019-10-02 20:10:02 -0600

    display form submitted data on screen on submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,36 @@ export class AppComponent {
   defaultSelectValue = "pet";
   answer: string = "";
   genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    questionAnswer: "",
+    gender: ""
+  };
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
+
+    // can use setvalue to set the whole form:
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ""
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male"
+    // });
+
+    // overwrite or set parts of the form:
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -22,7 +49,14 @@ export class AppComponent {
   // }
   // alternative approach using @ViewChild to access form instead of passig it in
   onSubmit() {
-    // access the form via the view child prop
-    console.log(this.signupForm);
+    this.submitted = true;
+
+    const { value } = this.signupForm;
+
+    this.user.username = value.userData.username;
+    this.user.email = value.userData.email;
+    this.user.secretQuestion = value.secret;
+    this.user.questionAnswer = value.questionAnswer;
+    this.user.gender = value.gender;
   }
 }
