ad2177b16d13fe7e4f407568d69d60b8c36c7e5d
Brent Marquez <brentonmarquez@gmail.com>
2019-10-02 19:32:03 -0600

    group form controls using ngModelGroup directive"\

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   @ViewChild("f", { static: true }) signupForm: NgForm;
   defaultSelectValue = "pet";
   answer: string = "";
+
   suggestUserName() {
     const suggestedName = "Superuser";
   }
