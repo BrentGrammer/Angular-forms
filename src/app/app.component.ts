89ce55ff79bf9521e746eb43c1c2d7dfc50659c9
Brent Marquez <brentonmarquez@gmail.com>
2019-10-02 19:39:57 -0600

    add radio buttons to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   @ViewChild("f", { static: true }) signupForm: NgForm;
   defaultSelectValue = "pet";
   answer: string = "";
+  genders = ["male", "female"];
 
   suggestUserName() {
     const suggestedName = "Superuser";
